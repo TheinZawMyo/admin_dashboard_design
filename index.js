3d64f9c28b914aa517feecefebf85f7f35e23233
Thein Zaw Myo <theinzawmyo40@gmail.com>
2021-12-16 09:32:37 +0630

    new design

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ toggleBtn.addEventListener('click', () => {
 closebtn.addEventListener('click', () => {
     menu.classList.remove('show_menu');
 });
+
+
+// privew image for upload 
+const img_input = document.getElementById('product_img');
+const img_preview = document.getElementById('product_img_preview');
+const preview_container = document.getElementById('preview_container');
+img_input.addEventListener("change", (e) => {
+    console.log(e.target.files)
+    if(e.target.files.length > 0 ){
+        let src = URL.createObjectURL(e.target.files[0]);
+        img_preview.src = src;
+        preview_container.style.display = 'block';
+    }
+})
